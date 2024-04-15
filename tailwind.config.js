aadb4c476ad9354237a1af43c88d8ca76f88ebc3
federudiero <federudiero@gmail.com>
2024-04-15 15:35:41 -0300

    responsive carrito terminado2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          bg: '#1a1a1a',
+          text: '#ffffff',
+          // Añade más colores si es necesario
+        },
+      },
+    },
   },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+    },
+  },
+  darkMode: 'class', // Habilita el modo oscuro basado en clases
   plugins:[require("daisyui")],
 }
-
