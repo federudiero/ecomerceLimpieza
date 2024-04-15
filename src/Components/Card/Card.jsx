d4eab23736adc6a73a0147cd495308974ce20339
federudiero <federudiero@gmail.com>
2024-04-15 15:44:45 -0300

    responsive carrito terminado3

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { setSelectedProductId, updateProductQuantity } from '../../redux/actions'; 
+import { setSelectedProductId, addToCart } from '../../redux/actions'; 
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { addToCart } from '../../redux/actions';
 
 function Card({ id, nombre, category, precio, imagen, stock }) {
-  console.log(id, nombre, category,precio, imagen, stock);
   const dispatch = useDispatch();
   const selectedProductQuantity = useSelector(state => state.selectedProductQuantity);
-  const [count, setCount] = useState(selectedProductQuantity);
 
   const handleViewDetails = () => {
     dispatch(setSelectedProductId(id));
   };
-
 
   const handleAddToCart = () => {
     const product = { id, nombre, category, precio, imagen, stock };
@@ -23,7 +19,7 @@ function Card({ id, nombre, category, precio, imagen, stock }) {
   };
 
   return (
-    <div className="card w-56 bg-base-100 shadow-md m-2 flex flex-col ">
+    <div className="dark card w-56 bg-gray-800 shadow-md m-2 flex flex-col">
       <div className="w-full h-44 overflow-hidden flex items-center justify-center border-primary">
         <img className="w-full h-full object-cover" src={imagen} alt="Producto" />
       </div>
@@ -35,28 +31,26 @@ function Card({ id, nombre, category, precio, imagen, stock }) {
         gap: '0.1rem',
         justifyContent: 'space-evenly'
       }}>
-        <h2 className="text-white ">{nombre}</h2>
+        <h2 className="text-white">{nombre}</h2>
         <h2 className="text-white mb-2">$ {precio}</h2>
-        <h4 className=" mb-2">{category}</h4>
+        <h4 className="text-white mb-2">{category}</h4>
       </div>
       <Link
-  to={`/product-detail/${id}`}
-  className="btn"
-  onClick={handleViewDetails}
-  style={{ background: '#ffe600' , color: 'black',}} // Establecer el color usando el código hexadecimal
->
-  Ver detalle
-</Link>
-       
-<button
+        to={`/product-detail/${id}`}
+        className="btn"
+        onClick={handleViewDetails}
+        style={{ background: '#ffe600', color: 'black' }}
+      >
+        Ver detalle
+      </Link>
+      <button
         className="btn"
         onClick={handleAddToCart}
-        style={{ background: '#ffe600' , color: 'black',}} // Establecer el color usando el código hexadecimal
+        style={{ background: '#ffe600', color: 'black' }}
       >
         <AddShoppingCartIcon/>
         Agregar al carrito
       </button>
-       
     </div>
   );
 }
