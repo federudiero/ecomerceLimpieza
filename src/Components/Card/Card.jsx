46d01b74b36658cc8a38e66c1305a0428f0901c6
federudiero <federudiero@gmail.com>
2024-05-28 18:11:14 -0300

    cambiosE-commerce

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -16,9 +16,9 @@ function Card({ id, nombre, category, precio, url }) {
   };
 
   return (
-    <div className="w-48 rounded overflow-hidden shadow-lg bg-white flex flex-col justify-between transform transition duration-300 hover:shadow-2xl hover:scale-105">
+    <div className="w-full sm:w-48 rounded overflow-hidden shadow-lg bg-white flex flex-col justify-between transform transition duration-300 hover:shadow-2xl hover:scale-105">
       <div className="flex-grow">
-        <img className="w-full h-48 object-cover" src={url} alt="Product" />
+        <img className="w-full h-48 object-contain sm:object-cover" src={url} alt="Product" />
         <div className="px-4 py-2">
           <div className="font-bold text-lg mb-2 text-gray-800">{nombre}</div>
           <p className="text-gray-600 text-sm mb-2">{category}</p>
@@ -26,13 +26,13 @@ function Card({ id, nombre, category, precio, url }) {
         </div>
       </div>
       <div className="px-4 py-2">
-      <button
-  className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded-full focus:outline-none flex items-center justify-center shadow-md transition duration-300 transform hover:scale-105"
-  onClick={handleAddToCart}
->
-  <AddShoppingCartIcon className="mr-2" />
-  <span className="text-gray-800 text-sm">Agregar al carrito</span>
-</button>
+        <button
+          className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded-full focus:outline-none flex items-center justify-center shadow-md transition duration-300 transform hover:scale-105"
+          onClick={handleAddToCart}
+        >
+          <AddShoppingCartIcon className="mr-2" />
+          <span className="text-gray-800 text-sm">Agregar al carrito</span>
+        </button>
         <Link
           to={`/product-detail/${id}`}
           className="text-blue-500 hover:underline focus:outline-none mt-4 flex items-center justify-center"
